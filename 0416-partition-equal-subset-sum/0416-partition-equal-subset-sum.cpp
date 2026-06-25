b5ae5df4d292066d8641e2a653d61c26624f0552
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-06-25 15:32:28 +0530

    Time: 55 ms (91.93%), Space: 15.5 MB (74.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,13 +2,8 @@ class Solution {
 public:
     bool help(int idx,vector<int>&nums,int sum,int cur, vector<vector<int>>&dp)
     {
-        if(sum-cur==cur) return true;
-        if(idx==0)
-        {
-            bool nottake=(sum-cur==cur);
-            bool take=(sum-(cur+nums[idx])==(cur+nums[idx]));
-            return nottake||take;
-        }
+        if(idx<0) return false;
+        if(sum-cur==cur&&idx>=0) return true;
         if(dp[idx][cur]!=-1) return dp[idx][cur];
         dp[idx][cur]=(help(idx-1,nums,sum,cur+nums[idx],dp)||help(idx-1,nums,sum,cur,dp));
         return dp[idx][cur];
@@ -16,7 +11,25 @@ public:
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=accumulate(nums.begin(),nums.end(),0);
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return help(n-1,nums,sum,0,dp);
+        if(sum%2!=0) return false;
+        sum/=2;
+       vector<int>prev(sum+1,0);
+       vector<int>curr(sum+1,0);
+       prev[0]=1;
+       curr[0]=1;
+        if(nums[0] <= sum)
+            prev[nums[0]] = 1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                if(j<nums[i])
+                curr[j]=prev[j];
+                else
+                curr[j] = (prev[j]||prev[j-nums[i]]);
+            }
+            prev=curr;
+        }
+        return prev[sum];
     }
 };
