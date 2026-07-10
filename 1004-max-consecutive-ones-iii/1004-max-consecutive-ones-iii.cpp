c595d3dc5e532c43701961c5b48151e699882e82
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-07-10 19:54:12 +0530

    Time: 0 ms (100%), Space: 69.5 MB (60.5%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,13 +5,12 @@ public:
         int i=0,j=0,c=0,ans=0;
         while(j<n)
         {
+           if(nums[j]==0)c++;
            while(c>k)
            {
              if(nums[i]==0) c--;
              i++;
            }
-           if(nums[j]==0)c++;
-           if(c<=k)
            ans=max(ans,j-i+1);
            j++;
         }
