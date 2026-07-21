551e00f0beec14a0a5cc1e0766e5dffaef80b087
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-07-21 23:25:03 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.79%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans=nums[0],curr=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            curr=max(curr+nums[i],nums[i]);
+            ans=max(ans,curr);
+        }
+        return ans;
+    }
+};
