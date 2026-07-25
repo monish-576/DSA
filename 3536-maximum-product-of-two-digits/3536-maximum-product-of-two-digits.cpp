24eeb67a98cf8443f54e25b5e30532fae4fb55fc
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-07-25 22:35:58 +0530

    Time: 0 ms (100%), Space: 9.2 MB (13.4%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int maxProduct(int n) {
       vector<long long>ans;
-      unordered_map<long long,int>mp;
       while(n>0)
       {
         ans.push_back(n%10);
-        mp[n%10]++;
         n/=10;
       }
       sort(ans.begin(),ans.end());
       int s=ans.size();
-      if(mp[ans[s-1]]>1)
-      return ans[s-1]*ans[s-1];
-      else
       return ans[s-1]*ans[s-2];
     }
 };
