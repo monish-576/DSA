0946c30dc10562e328ace57be89d5ae43d070c36
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-08-03 23:49:04 +0530

    Time: 3 ms (33.86%), Space: 10.3 MB (23.82%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int help(vector<int>&nums,int i,int j,int p)
+    int help(vector<int>&nums,int i,int j,int p,vector<vector<int>>&dp)
     {
         if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
         if(p==0)
         {
-            int s1=nums[i]+help(nums,i+1,j,1);
-            int s2=nums[j]+help(nums,i,j-1,1);
-            return max(s1,s2);
+            int s1=nums[i]+help(nums,i+1,j,1,dp);
+            int s2=nums[j]+help(nums,i,j-1,1,dp);
+            return dp[i][j]=max(s1,s2);
         }
         else
         {
-            int s1=help(nums,i+1,j,0);
-            int s2=help(nums,i,j-1,0);
-            return min(s1,s2);
+            int s1=help(nums,i+1,j,0,dp);
+            int s2=help(nums,i,j-1,0,dp);
+            return dp[i][j]=min(s1,s2);
         }
     }
     bool predictTheWinner(vector<int>& nums) {
         int tot=accumulate(nums.begin(),nums.end(),0);
-        int s1=help(nums,0,nums.size()-1,0);
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int s1=help(nums,0,nums.size()-1,0,dp);
         int s2=tot-s1;
         return s1>=s2;
     }
