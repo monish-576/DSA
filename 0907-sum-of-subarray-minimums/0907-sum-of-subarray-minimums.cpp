07ad93bda4948c9e14ce786ff9c532b40f641013
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-08-07 10:46:17 +0530

    Time: 121 ms (6.75%), Space: 202.2 MB (11.34%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,52 +1,47 @@
 class Solution {
 public:
-    void help1(vector<int>&arr,vector<int>&pse)
+    void ps(vector<int>&arr,vector<int>&pse)
     {
-        int n=arr.size();
-        stack<int>s;
-        for(int i=0;i<n;i++)
+        stack<pair<int,int>>s;
+        for(int i=0;i<arr.size();i++)
         {
-           while(!s.empty()&&arr[s.top()]>arr[i])
-           {
-              s.pop();
-           }
-           if(s.empty())
-           pse[i]=-1;
-           else
-           pse[i]=s.top();
-           s.push(i);
+            while(!s.empty()&&s.top().first>arr[i])
+            {
+                s.pop();
+            }
+            if(s.empty()) pse[i]=-1;
+            else pse[i]=s.top().second;
+            s.push({arr[i],i});
         }
     }
-    void help2(vector<int>&arr,vector<int>&nse)
+     void ns(vector<int>&arr,vector<int>&nse)
     {
-        int n=arr.size();
-        stack<int>s;
-        for(int i=n-1;i>=0;i--)
+        stack<pair<int,int>>s;
+        for(int i=arr.size()-1;i>=0;i--)
         {
-           while(!s.empty()&&arr[s.top()]>=arr[i])
-           {
-              s.pop();
-           }
-           if(s.empty())
-           nse[i]=n;
-           else
-           nse[i]=s.top();
-           s.push(i);
+            while(!s.empty()&&s.top().first>=arr[i])
+            {
+                s.pop();
+            }
+            if(s.empty()) nse[i]=arr.size();
+            else nse[i]=s.top().second;
+            s.push({arr[i],i});
         }
     }
     int sumSubarrayMins(vector<int>& arr) {
+        const long long mod=1e9+7;
         int n=arr.size();
-        int mod=1e9+7;
-        vector<int>pse(n),nse(n);
-        help1(arr,pse);
-        help2(arr,nse);
-        long long tot=0;
-        for(int i=0;i<n;i++)
+        vector<int>nse(n),pse(n);
+        long long sum=0;
+        ps(arr,pse);
+        ns(arr,nse);
+        for(int i=0;i<arr.size();i++)
         {
-            long long left=i-pse[i];
-            long long right=nse[i]-i;
-            tot=(tot+(left*right*arr[i])%mod)%mod;
+            long long start=i-pse[i];
+            long long end=nse[i]-i;
+            long long curr = (end*(start*arr[i])%mod)%mod;
+            sum=(sum+curr)%mod;
         }
-        return (int)tot;
+        return (int)sum;
     }
 };
