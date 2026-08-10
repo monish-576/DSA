5f243b6c1e51f9b1fcf29ac7ce8c4638c1fc5668
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-08-11 00:12:39 +0530

    Time: 0 ms (100%), Space: 9.3 MB (45.62%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void solve(int idx,string &digits,vector<string>&ans,string &temp,string s[])
+    void help(int idx,string digits,vector<string>&s,string res,vector<string>&ans)
     {
         if(idx==digits.size())
         {
-            ans.push_back(temp);
+            ans.push_back(res);
             return;
         }
         for(int i=0;i<s[digits[idx]-'0'].size();i++)
         {
-            char curr=s[digits[idx]-'0'][i];
-            temp.push_back(curr);
-            solve(idx+1,digits,ans,temp,s);
-            temp.pop_back();
+            res+=s[digits[idx]-'0'][i];
+            help(idx+1,digits,s,res,ans);
+            res.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-       string temp;
-       vector<string>ans;
-       string s[]={" "," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-       solve(0,digits,ans,temp,s);
-       return ans;
+        vector<string>s={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string>ans;
+        string res="";
+        help(0,digits,s,res,ans);
+        return ans;
     }
 };
