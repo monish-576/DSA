5f291fb28c385a76e87e06af68d352cf9f4bd060
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-08-11 15:55:02 +0530

    Time: 0 ms (100%), Space: 32.6 MB (83.73%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -3,22 +3,25 @@ public:
     int help(vector<int>&nums,int goal)
     {
         if(goal<0) return 0;
-        int i=0,j=0,n=nums.size(),sum=0,ans=0;
+        int ans=0;
+        int n=nums.size();
+        int i=0,j=0;
+        int sum=0;
         while(j<n)
         {
             sum+=nums[j];
             while(sum>goal)
             {
-                sum=sum-nums[i];
-                i++;
+               sum=sum-nums[i];
+               i++;
             }
             if(sum<=goal)
-            ans+=j-i+1;
+            ans+=(j-i+1);
             j++;
         }
         return ans;
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return help(nums,goal)-help(nums,goal-1);
+      return help(nums,goal)-help(nums,goal-1);
     }
 };
