3b6a201ea5086173cb945bca0cc91f59a2d2dbbe
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-09-17 01:39:23 +0530

    Time: 4 ms (57.08%), Space: 15.9 MB (26.62%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        string o=s;
-        string str=s;
-        reverse(s.begin(),s.end());
-        str+='#';
-        str+=s;
-        int n=str.size();
-        vector<int>lps(n,0);
-        int len=0,i=1;
-        while(i<n)
-        {
-            if(str[i]==str[len])
-            {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        string str = s + '#' + rev;
+
+        int n = str.size();
+        vector<int> lps(n, 0);
+
+        int len = 0, i = 1;
+
+        while (i < n) {
+            if (str[i] == str[len]) {
                 len++;
-                lps[i]=len;
+                lps[i] = len;
                 i++;
             }
-            else
-            {
-                if(len!=0) len=lps[len-1];
-                else i++;
+            else {
+                if (len != 0)
+                    len = lps[len - 1];
+                else
+                    i++;
             }
         }
-        string ans=o.substr(0,lps[n-1]);
-        string p=o.substr(lps[n-1],n-lps[n-1]);
-        ans+=p;
-        reverse(p.begin(),p.end());
-        p+=ans;
-        return p;
+
+        int palLen = lps[n - 1];
+
+        string add = s.substr(palLen);
+        reverse(add.begin(), add.end());
+
+        return add + s;
     }
 };
